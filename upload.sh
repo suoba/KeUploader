2647b913e8eb286ec0495805da43f470deeaa624
suoba <sumat1984@gmail.com>
2016-10-11 16:20:12 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/cdhigh/KindleEar.git
+git clone https://github.com/suoba/KindleEar.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
